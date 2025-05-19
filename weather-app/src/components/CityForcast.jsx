d9800ad2ae7b4969fe1661a6971cc4fdf52df29c
BlueRasputin <110371787+BlueRasputin@users.noreply.github.com>
2025-05-19 00:42:02 -0500

    Update CityForcast.jsx
    ignore, switched to different file because of typo in this one

--- weather-app/src/components/CityForcast.jsx
+++ weather-app/src/components/CityForcast.jsx
@@ -1,30 +1,32 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import * as cities from '../data/weather-data.json';
-import CityList from './CityList';
+//switched to a different file because i made a typo when naming this one
 
-function CityForecast() {
-  const { cityName } = useParams();
-  const city = cities.find((c) => c.name === cityName);
+//import React, { useState, useRef, useEffect } from 'react';
+//import { useParams, Link } from 'react-router-dom';
+//import * as cities from '../data/weather-data.json';
+//import CityList from './CityList';
 
-  if (!cityName || !city) { 
-    return (
-      <div className="container">
-        <h1 className="title">Weather data not available for this city</h1>
-        <p className="error">Invalid or missing city.</p>
-        <Link to="/" className="back-link">Back to City List</Link>
-      </div>
-    );
-  }
-  return (
-    <div className="container">
-      <h1 className="name">{city.name}</h1>
-      <p className="summary">{city.summary}</p>
-      
-      <p className ="details">{city.details}</p>
-      
-      <Link to="/" className="back-link">Back to City List</Link>
-    </div>
-  );
-}
-export default CityForcast;
+//function CityForecast() {
+//  const { cityName } = useParams();
+//  const city = cities.find((c) => c.name === cityName);
+
+//  if (!cityName || !city) { 
+//    return (
+//      <div className="container">
+//        <h1 className="title">Weather data not available for this city</h1>
+//        <p className="error">Invalid or missing city.</p>
+//        <Link to="/" className="back-link">Back to City List</Link>
+//      </div>
+//    );
+//  }
+//  return (
+//    <div className="container">
+//      <h1 className="name">{city.name}</h1>
+//      <p className="summary">{city.summary}</p>
+//      
+//      <p className ="details">{city.details}</p>
+//      
+//      <Link to="/" className="back-link">Back to City List</Link>
+//    </div>
+//  );
+//}
+//export default CityForcast;
